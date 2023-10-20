d3cc3564214a33de5f741713b8b2f1f0f2c4bbcc
Polina <p9991820@gmail.com>
2023-10-20 03:16:46 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,56 @@
+    for item in data:
+    data = generate_random_data()
+
+def main():
+
+if __name__ == "__main__":
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+    return data
+    data = generate_random_data()
+if __name__ == "__main__":
+
+    main()
+    return data
+
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+import random
+def generate_random_data():
+
+def main():    return data
+def main():
+
+    main()
+import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+if __name__ == "__main__":
+
+
+    for item in data:
+        print(f"Random Number: {item}")
+def generate_random_data():function generateRandomData() {
+}
+
+    const data = [];
+main();
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+function main() {
+
+    });
+    data.forEach(item => {
+
+    return data;
+    for (let i = 0; i < 10; i++) {
+}
+        console.log("Random Number: " + item);
