682daa92f5ff13c2a910b3e88540e94e0d0e6440
Polina <p9991820@gmail.com>
2023-10-22 15:51:55 +0300

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,56 +1,53 @@
-    for item in data:
     data = generate_random_data()
 
-def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
 
 if __name__ == "__main__":
 def generate_random_data():
-
-        print(f"Random Number: {item}")
 import random
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-    return data
-    data = generate_random_data()
-if __name__ == "__main__":
 
-    main()
     return data
-
+    for item in data:
+def main():
+    main()
         print(f"Random Number: {item}")
-
-    data = [random.randint(1, 100) for _ in range(10)]
     for item in data:
 import random
 def generate_random_data():
 
-def main():    return data
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+    data = generate_random_data()
 def main():
 
-    main()
+    return data
+        print(f"Random Number: {item}")    data = generate_random_data()
+    for item in data:
 import random
 
+
+
+        print(f"Random Number: {item}")
+def generate_random_data():
     data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
+    main()
+def main():
 if __name__ == "__main__":
+
+    return datadef main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+if __name__ == "__main__":
+    data = generate_random_data()
+def generate_random_data():
+import random
 
 
     for item in data:
+    return data
         print(f"Random Number: {item}")
-def generate_random_data():function generateRandomData() {
-}
-
-    const data = [];
-main();
-    const data = generateRandomData();
-        data.push(Math.floor(Math.random() * 100) + 1);
-    }
-function main() {
-
-    });
-    data.forEach(item => {
-
-    return data;
-    for (let i = 0; i < 10; i++) {
-}
-        console.log("Random Number: " + item);
